762d073f446e5fa9972cbebe06aab00947f07a2c
Nebil Shibeshi <narega678@gmail>
2025-06-07 18:07:14 +0300

    fix: remove border from primary button

--- src/components/common/Buttons/PrimaryButton.tsx
+++ src/components/common/Buttons/PrimaryButton.tsx
@@ -18,7 +18,7 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({
         isSelected
           ? "bg-blue-500 hover:bg-blue-600 dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-white"
           : "bg-black hover:bg-white dark:bg-gray-200  dark:hover:bg-black"
-      }  text-white hover:text-black border-black dark:text-black dark:hover:text-white justify-center rounded-md px-3 py-2.5 text-sm font-semibold leading-6 shadow-sm transition-all  ${className}`}
+      }  text-white hover:text-black dark:text-black dark:hover:text-white justify-center rounded-md px-3 py-2.5 text-sm font-semibold leading-6 shadow-sm transition-all  ${className}`}
     >
       {children}
     </button>
